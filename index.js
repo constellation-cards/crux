38ccdafa818cf1deb7275090634e278faa929ff4
Bill Garrett <garrett@astralfrontier.org>
2024-11-26 20:15:32 -0800

    Respect the CRUX_CARDS_DECK variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const ejs = require("ejs");
 const fs = require("fs");
 const ramda = require("ramda");
 
-const { concat, isEmpty, isNil, pick, reduce, repeat } = ramda;
+const { concat, isEmpty, isNil, isNotEmpty, isNotNil, pick, reduce, repeat } = ramda;
 
 // Turn an array of records into a map of UID => [Record, Record...]
 function recordsToMap(records) {
@@ -27,7 +27,7 @@ const decks = recordsToMap(cardData.decks);
 const stacks = recordsToMap(cardData.stacks);
 
 // Dereference decks and stacks and bake them into our structure
-const cards = reduce(
+const allCards = reduce(
   (cards, card) => {
     // Uncomment to duplicate cards based on quantity
     // We want this if we're printing cards, but not for TTS
@@ -42,6 +42,13 @@ const cards = reduce(
   [],
   cardData.cards
 );
+
+let cards
+if (isNotNil(process.env.CRUX_CARDS_DECK) && isNotEmpty(process.env.CRUX_CARDS_DECK)) {
+  cards = allCards.filter(card => card.deck.name == process.env.CRUX_CARDS_DECK);
+} else {
+  cards = allCards
+}
 
 const description = (inputString) => {
   const lines = inputString.split("\n").map(line => {
